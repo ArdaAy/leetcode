817dc7c569cbd9a1dadc9a1bfdfa3d3242710b18
Arda AY <arda.ay@outlook.com.tr>
2023-11-28 19:35:50 +0000

    Sync LeetCode submission - Customer Who Visited but Did Not Make Any Transactions (mssql)

---
+++ problems/customer_who_visited_but_did_not_make_any_transactions/solution.sql
@@ -0,0 +1,8 @@
+/* Write your T-SQL query statement below */
+SELECT 
+    t1.customer_id, 
+    COUNT(t1.visit_id) as count_no_trans
+FROM Visits t1
+LEFT JOIN Transactions t2 ON t1.visit_id = t2.visit_id
+WHERE t2.transaction_id IS NULL
+GROUP BY t1.customer_id
