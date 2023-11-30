dfb6db6a4ca7f4959b0dc5440db0ee31b758f4d6
Arda AY <arda.ay@outlook.com.tr>
2023-11-30 22:39:04 +0000

    Sync LeetCode submission - Remove Element (csharp)

--- problems/remove_element/solution.cs
+++ problems/remove_element/solution.cs
@@ -1,19 +1,13 @@
 public class Solution {
     public int RemoveElement(int[] nums, int val) {
         List<int> iList = new List<int>();
-        var cnt = 0;
-
-        for (var i = 0; i < nums.Length; i++)
-        {
-            if (nums[i] != val)
-            {
+        for(var i = 0; i < nums.Length; i++){
+            if(nums[i] != val)
                 iList.Add(nums[i]);
-                cnt++;
-            }
         }
-        for (var i = 0; i < iList.Count; i++)
+        for(var i = 0; i < iList.Count; i++)
             nums[i] = iList[i];
-            
-        return cnt;
+        
+        return iList.Count;
     }
 }
