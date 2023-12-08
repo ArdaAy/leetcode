ae0c74029276a221a459c35c5ca61b8d1684515d
Arda AY <arda.ay@outlook.com.tr>
2023-12-08 16:19:17 +0000

    Sync LeetCode submission - Reverse String (csharp)

--- problems/reverse_string/solution.cs
+++ problems/reverse_string/solution.cs
@@ -1,10 +1,21 @@
 public class Solution {
     public void ReverseString(char[] s) {
+        var i = 0;
+        var j = s.Length - 1;
+        while(i<j){
+            var tmp = s[i];
+            s[i] = s[j];
+            s[j] = tmp;
+            i++;
+            j--;
+        }
+        /*
         var ss = new char[s.Length];
         var cnt = s.Length - 1;
         for (var i = 0; i < s.Length; i++, cnt--)
             ss[i] = s[cnt];
         for (var i = 0; i < s.Length; i++)
             s[i] = ss[i];
+        */
     }
 }
